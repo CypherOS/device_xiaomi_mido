283ce966a927dc7f2de2d97845169566215faa03
Chris <chris@cypheros.co>
2017-08-08 20:59:11 -0400

    mido: add supported hardware overlays
    Remove unsupported ones as well
    
    Change-Id: I30f02060b50ec432bffa15da0f2c591d2ceadf2b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,6 +108,8 @@ TARGET_TS_MAKEUP := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
+
 # CNE / DPM
 BOARD_USES_QCNE := true
 
