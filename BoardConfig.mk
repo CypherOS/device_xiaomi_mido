273fe9ff5ab45d0dd95ce291d23436d5fbfb40ef
TheScarastic <warabhishek@gmail.com>
2018-03-24 15:21:20 +0530

    mido: Add aux supported apps list and enable hal3
    Change-Id: Ib3dfa8ac3b3e9d45c5fe57bd28eed4f927349c98

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,9 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
+# Properties
+TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
+
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 PRODUCT_FULL_TREBLE_OVERRIDE := true
